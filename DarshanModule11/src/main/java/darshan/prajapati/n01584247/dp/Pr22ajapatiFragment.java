e45583cc914ba0074a414e1089b865f2ae437365
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2024-04-07 21:02:13 -0400

    Integrated the google map with specified location.

--- DarshanModule11/src/main/java/darshan/prajapati/n01584247/dp/Pr22ajapatiFragment.java
+++ DarshanModule11/src/main/java/darshan/prajapati/n01584247/dp/Pr22ajapatiFragment.java
@@ -1,33 +1,49 @@
 package darshan.prajapati.n01584247.dp;
 
-import android.os.Bundle;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Pr22ajapatiFragment extends Fragment {
 
+    private final LatLng CAMBAY = new LatLng(22.3181, 72.6194);
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
-    public Pr22ajapatiFragment() {
-        // Required empty public constructor
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            googleMap.addMarker(new MarkerOptions().position(CAMBAY).title(getString(R.string.map_location))
+                    .snippet(getString(R.string.map_snippet)));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(CAMBAY, 15));
+        }
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_pr22ajapati, container, false);
     }
 
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_pr22ajapati, container, false);
-        // Inflate the layout for this fragment
-        return view;
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.DarGmap);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
     }
 }
