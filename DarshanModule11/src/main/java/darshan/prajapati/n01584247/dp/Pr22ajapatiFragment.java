72b168d57789dbf04670566f6b038309f35a499a
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2024-04-07 22:01:02 -0400

    Implemented geocoder and handled the setOnMapClickListener.

--- DarshanModule11/src/main/java/darshan/prajapati/n01584247/dp/Pr22ajapatiFragment.java
+++ DarshanModule11/src/main/java/darshan/prajapati/n01584247/dp/Pr22ajapatiFragment.java
@@ -4,10 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,6 +18,12 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
 
 public class Pr22ajapatiFragment extends Fragment {
 
@@ -26,6 +35,14 @@ public class Pr22ajapatiFragment extends Fragment {
             googleMap.addMarker(new MarkerOptions().position(CAMBAY).title(getString(R.string.map_location))
                     .snippet(getString(R.string.map_snippet)));
             googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(CAMBAY, 15));
+
+            googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                @Override
+                public void onMapClick(LatLng latLng) {
+                    getAddressFromLatLng(latLng, googleMap);
+                }
+            });
+
         }
     };
 
@@ -45,5 +62,50 @@ public class Pr22ajapatiFragment extends Fragment {
         if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
         }
+    }
+
+    private void getAddressFromLatLng(LatLng latLng, GoogleMap googleMap) {
+        Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
+
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+
+            if (addresses != null && addresses.size() > 0) {
+                Address address = addresses.get(0);
+                String addressText = String.format("%s, %s, %s",
+                        address.getAddressLine(0),
+                        address.getLocality(),
+                        address.getCountryName());
+
+                // Update TextView
+                TextView fullNameTextView = requireActivity().findViewById(R.id.DarAboveMapTV);
+                fullNameTextView.setText(addressText);
+
+                googleMap.clear(); // Clear previous markers
+                googleMap.addMarker(new MarkerOptions().position(latLng).title("Darshan Prajapati")
+                        .snippet(addressText));
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+
+                // Show Snack bar
+                showSnackbar(addressText);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showSnackbar(String addressText) {
+        Snackbar snackbar = Snackbar.make(getActivity().findViewById(android.R.id.content),
+                addressText, Snackbar.LENGTH_INDEFINITE);
+
+        snackbar.setAction(R.string.dismiss, new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                snackbar.dismiss();
+            }
+        });
+
+        snackbar.show();
     }
 }
